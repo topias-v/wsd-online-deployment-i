45878f727107d6cb34c15b7a3a511c00014ade04
topias <topias.vesanen@hotmail.com>
2023-10-30 15:30:54 +0200

    starting to use view templates

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 import { serve } from "./deps.js";
+import { configure, renderFile } from "./deps.js";
 
-const handleRequest = (request) => {
-  return new Response("Seeking truths beyond meaning of life, you will find 43.");
+configure({
+  views: `${Deno.cwd()}/views/`,
+});
+
+const responseDetails = {
+  headers: { "Content-Type": "text/html;charset=UTF-8" },
+};
+
+const data = {
+  count: 0,
+};
+
+const handleRequest = async (request) => {
+  const url = new URL(request.url);
+  if (url.pathname === "/visits") {
+    data.count++;
+    return new Response(await renderFile("count.eta", data), responseDetails);
+  } else if (url.pathname === "/meaning") {
+    return new Response("Seeking truths beyond meaning of life, you will find 43.");
+  } else {
+    return new Response("Hello you!");
+  }
 };
 
 serve(handleRequest, { port: 7777 });
