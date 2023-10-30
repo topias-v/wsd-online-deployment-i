f0b7ca3e3dba9ff2a5a550d7590b9441b2b22aa7
topias <topias.vesanen@hotmail.com>
2023-10-30 15:01:18 +0200

    Changed response

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { serve } from "./deps.js";
 
 const handleRequest = (request) => {
-  return new Response("Hello world!");
+  return new Response("Seeking truths beyond meaning of life, you will find 43.");
 };
 
 serve(handleRequest, { port: 7777 });
